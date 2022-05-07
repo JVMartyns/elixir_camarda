4e5eaf69bc4040bc09aa5c53109f0bd34a288382
JVMartyns <jvmartyns2015@gmail.com>
2022-05-07 08:44:32 -0300

    Created: route to create trainer

--- web/ex_mon/lib/ex_mon_web/router.ex
+++ web/ex_mon/lib/ex_mon_web/router.ex
@@ -7,6 +7,8 @@ defmodule ExMonWeb.Router do
 
   scope "/api", ExMonWeb do
     pipe_through :api
+
+    resources "/trainers", TrainersController, only: [:create, :show, :delete, :update]
   end
 
   # Enables LiveDashboard only for development
