dc911232a9337821016b1eb2f619bbc108f2a8af
JVMartyns <jvmartyns2015@gmail.com>
2022-05-09 13:40:10 -0300

    Createed: route to GET pokemon

--- web/ex_mon/lib/ex_mon/pokemon.ex
+++ web/ex_mon/lib/ex_mon/pokemon.ex
@@ -2,7 +2,8 @@ defmodule ExMon.Pokemon do
   @keys [:id, :name, :weight, :types]
   @enforce_keys @keys
 
-  defstruct @keys
+  @derive Jason.Encoder
+  defstruct(@keys)
 
   def build(%{"id" => id, "name" => name, "weight" => weight, "types" => types}) do
     %__MODULE__{
