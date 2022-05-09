a27ba59945518fe9917d92af37e33cd4514143c2
JVMartyns <jvmartyns2015@gmail.com>
2022-05-09 10:24:39 -0300

    Created: HTTP Client

--- web/ex_mon/mix.exs
+++ web/ex_mon/mix.exs
@@ -44,7 +44,8 @@ defmodule ExMon.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:argon2_elixir, "~> 3.0"}
+      {:argon2_elixir, "~> 3.0"},
+      {:tesla, "~> 1.4"}
     ]
   end
 
