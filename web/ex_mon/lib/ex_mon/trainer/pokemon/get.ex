d592fb77cbdf8d13123ea8711114c626ba42b274
JVMartyns <jvmartyns2015@gmail.com>
2022-05-11 11:22:53 -0300

    Created: get pokemon

---
+++ web/ex_mon/lib/ex_mon/trainer/pokemon/get.ex
@@ -0,0 +1,21 @@
+defmodule ExMon.Trainer.Pokemon.Get do
+  alias Ecto.UUID
+  alias ExMon.{Repo, Trainer.Pokemon}
+
+  def call(id) do
+    case UUID.cast(id) do
+      :error ->
+        {:error, "Invalid ID format!"}
+
+      {:ok, uuid} ->
+        get(uuid)
+    end
+  end
+
+  defp get(uuid) do
+    case Repo.get(Pokemon, uuid) do
+      nil -> {:error, "Trainer not found"}
+      pokemon -> {:ok, Repo.preload(pokemon, :trainer)}
+    end
+  end
+end
