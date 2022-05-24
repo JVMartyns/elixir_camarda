680f0ceb6a706f1b8c2e4412f6bc2155897104d0
JVMartyns <jvmartyns2015@gmail.com>
2022-05-24 10:50:50 -0300

    Created: tets for Trainer.Get module

---
+++ web/ex_mon/test/ex_mon/trainer/get_test.ex
@@ -0,0 +1,33 @@
+defmodule ExMon.Trainer.GetTest do
+  use ExMon.DataCase
+
+  alias ExMon.Trainer
+  alias Trainer.Delete
+
+  describe "call/1" do
+    test "When the ID is valid, return a trainer" do
+      params = %{name: "Vitor", password: "123456"}
+      ExMon.create_trainer(params)
+
+      {:ok, %Trainer{id: id}} = ExMon.create_trainer(params)
+
+      assert {:ok, %Trainer{id: ^id}} = ExMon.Trainer.Get.call(id)
+    end
+
+    test "When the ID is not valid, delete a trainer" do
+      id = "invalid_ID"
+
+      expected_response = {:error, "Invalid ID format!"}
+
+      assert Get.call(id) == expected_response
+    end
+
+    test "When the trainer not found" do
+      id = Ecto.UUID.generate()
+
+      expected_response = {:error, "Trainer not found"}
+
+      assert Get.call(id) == expected_response
+    end
+  end
+end
